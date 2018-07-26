f2d47bbe150c4c6c79d2710a2dbc38019bfc3db3
Monica Choe <monicchoe@gmail.com>
2018-07-26 17:48:10 -0400

    chaninging

--- summing-grid/src/App.js
+++ summing-grid/src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
       <div className="App">
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          Monica
         </p>
       </div>
     );
